23cb6a5a3e162bc9d47285425333b8fff627f06e
Tyler Blakeman <tylerblakeman@gmail.com>
2020-03-18 20:32:44 -0500

    add HTML, global vars, random word function

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+var wordArray = ["apple", "banana", "pineapple", "watermelon", "starfruit"]
+var livesRemaining = 9
+var wins = 0;
+var losses = 0;
+var playerInput
+var randomWord
+var wordBlanks
+var alphabetString = "abcdefghijklmnopqrstuvwxyz".split("")
+
+
+function getRandomWord(){
+    randomWord = wordArray[Math.floor(Math.random()*(wordArray.length))]
+    console.log(randomWord)
+    for 
+}
+
+getRandomWord();
